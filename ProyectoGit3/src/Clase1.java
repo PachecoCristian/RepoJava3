90bf2e1267e73e43c9a323b6e007cf5cc5c97aac
PachecoCristian <36259537+PachecoCristian@users.noreply.github.com>
2018-03-05 09:13:27 +0100

    Update Clase1.java

--- ProyectoGit3/src/Clase1.java
+++ ProyectoGit3/src/Clase1.java
@@ -4,6 +4,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Holaczoy pogramador");
 		System.out.println("Adios <>");
+		//Un comentario escrito en gitHub
 	}
 
 }
